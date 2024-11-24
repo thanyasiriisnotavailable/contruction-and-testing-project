4f7e177bbce787810746e3b55fbc7b46719c34cb
thanyasiriisnotavailable <thanyasiri_ch@cmu.ac.th>
2024-11-24 15:55:14 +0700

    Provide the specific folder

--- controllers/navbar/categoryDropdownController.js
+++ controllers/navbar/categoryDropdownController.js
@@ -35,24 +35,3 @@ document.addEventListener("DOMContentLoaded", function () {
     })
     .catch((error) => console.error("Error fetching categories:", error));
 });
-
-document.addEventListener("DOMContentLoaded", function () {
-  const exitFullscreenButton = document.getElementById("exitFullscreenButton");
-  const navbarCollapse = document.querySelector(".navbar-collapse");
-
-  exitFullscreenButton.style.display = "none";
-
-  const navbarToggler = document.querySelector(".navbar-toggler");
-  navbarToggler.addEventListener("click", function () {
-    if (!navbarCollapse.classList.contains("show")) {
-      exitFullscreenButton.style.display = "block";
-    } else {
-      exitFullscreenButton.style.display = "none";
-    }
-  });
-
-  exitFullscreenButton.addEventListener("click", function () {
-    navbarCollapse.classList.remove("show");
-    exitFullscreenButton.style.display = "none";
-  });
-});
