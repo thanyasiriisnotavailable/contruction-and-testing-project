91ce390ef03d94834a9977744f25dc9f3d0af99a
Cxttonn <phitchayapha_b@cmu.ac.th>
2024-11-26 15:08:37 +0700

    Update html, CSS, and model part
    remove css from register.html and ourstore.html to style.css, remove some unnecessary comment, and rename html class

--- models/wishlistModel.js
+++ models/wishlistModel.js
@@ -2,12 +2,10 @@ const express = require('express');
 const router = express.Router();
 const WishDB = require('../models/wishModel');
 
-// Route to handle adding a product to the wishlist
 router.post('/addwishlist', async (req, res) => {
     const { productId, productName, productPrice, productImage } = req.body;
 
     try {
-        // Get user's wishlist
         const userWishlist = await WishDB.getUserWishlist(req.session.userId);
 
         // Add the product details to the wishlist
@@ -21,10 +19,8 @@ router.post('/addwishlist', async (req, res) => {
         // Save the updated wishlist
         await WishDB.updateUserWishlist(req.session.userId, userWishlist);
 
-        // Send success response
         res.send("Wishlist updated successfully");
     } catch (error) {
-        // Handle errors
         console.error("Error updating wishlist:", error);
         res.status(500).send("An error occurred while updating the wishlist");
     }
